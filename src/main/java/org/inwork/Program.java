e057aa7274bea1ea476dbe53d3eb7c5216dfb796
oleg <legik7127@gmail.com>
2023-06-21 17:35:31 +0600

    added method writeFileContents()

--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -1,5 +1,8 @@
 package org.inwork;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.Random;
 
 public class Program {
@@ -18,5 +21,15 @@ public class Program {
             stringBuilder.append((char) random.nextInt(CHAR_BOUND_L,CHAR_BOUND_H+1));
         }
         return stringBuilder.toString();
+    }
+    private static void writeFileContents(String filename, int length) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(filename)){
+            fileOutputStream.write(generateSymbols(length).getBytes());
+
+        }
+//        FileOutputStream fileOutputStream = new FileOutputStream(filename);
+//        fileOutputStream.write(generateSymbols(length).getBytes());
+//        fileOutputStream.flush();
+//        fileOutputStream.close();
     }
 }
