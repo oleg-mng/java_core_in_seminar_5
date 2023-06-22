36a4d70c3bff78e3be4bfed79ba6b878b8afd4ed
oleg <legik7127@gmail.com>
2023-06-22 17:56:19 +0600

    added output method to file system hierarchy console outputFileSystem()

--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -1,8 +1,6 @@
 package org.inwork;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Random;
 
 public class Program {
@@ -15,7 +13,27 @@ public class Program {
     public static void main(String[] args) throws IOException {
         System.out.println(generateSymbols(15));
         writeFileContents("s5sample.txt", 30);
+        System.out.println(searchStrInFile("s5sample.txt", TO_SEARCH));
         writeFileContents2("s5sample2.txt", 30, 5);
+        System.out.println(searchStrInFile("s5sample2.txt", TO_SEARCH));
+        concatenate("s5sample.txt", "s5sample2.txt","s5sampleOut.txt");
+        System.out.println(searchStrInFile("s5sampleOut.txt", TO_SEARCH));
+
+    }
+
+    public static void outputFileSystem(File file, String indent, boolean isLast){
+        System.out.println(indent);
+        if (isLast) {
+            System.out.print("└ ");
+            indent += "  ";
+        }
+        else {
+            System.out.print("├");
+            indent += "│ ";
+        }
+        System.out.println(file.getName());
+        File[] files = file.listFiles();
+
 
     }
 
@@ -47,6 +65,40 @@ public class Program {
                 } else fileOutputStream.write(generateSymbols(length).getBytes());
             }
             fileOutputStream.write(' ');
+
+        }
+    }
+    private static void concatenate(String fileIn1, String fileIn2, String fileOut) throws IOException{
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)){
+            int c;
+            try (FileInputStream fileInputStream = new FileInputStream(fileIn1)){
+                while ((c = fileInputStream.read()) != -1){
+                    fileOutputStream.write(c);
+                }
+            }
+            try (FileInputStream fileInputStream = new FileInputStream(fileIn2)){
+                while ((c = fileInputStream.read()) != -1){
+                    fileOutputStream.write(c);
+                }
+            }
+        }
+    }
+    private static boolean searchStrInFile(String filename, String search) throws IOException{
+        try(FileInputStream fileInputStream = new FileInputStream(filename)){
+            byte[] searchData = search.getBytes();
+            int c;
+            int i = 0;
+            while ((c = fileInputStream.read()) != -1){
+                if (c == searchData[i]) {
+                    i++;
+                }
+                else {
+                    i = 0;
+                    if (c == searchData[i]) i++;
+                }
+                if ( i == searchData.length) return true;
+            }
+            return false;
 
         }
     }
