30f3b38347ee182757606ab4eee9c99a36d518e9
oleg <legik7127@gmail.com>
2023-06-24 17:53:56 +0600

    added searchMatch()

--- src/main/java/org/inwork/Program.java
+++ src/main/java/org/inwork/Program.java
@@ -1,6 +1,8 @@
 package org.inwork;
 
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Program {
@@ -10,33 +12,48 @@ public class Program {
     private static final String TO_SEARCH = "GeekBrains";
 
 
+
+
     public static void main(String[] args) throws IOException {
         System.out.println(generateSymbols(15));
         writeFileContents("s5sample.txt", 30);
         System.out.println(searchStrInFile("s5sample.txt", TO_SEARCH));
         writeFileContents2("s5sample2.txt", 30, 5);
         System.out.println(searchStrInFile("s5sample2.txt", TO_SEARCH));
-        concatenate("s5sample.txt", "s5sample2.txt","s5sampleOut.txt");
+        concatenate("s5sample.txt", "s5sample2.txt", "s5sampleOut.txt");
         System.out.println(searchStrInFile("s5sampleOut.txt", TO_SEARCH));
 
         outputFileSystem(new File("."), "", false);
 
+        String[] fileNames = new String[10];
+        for (int i = 0; i < fileNames.length; i++) {
+            fileNames[i] = "file_" + i + ".txt";
+            writeFileContents2(fileNames[i],100, 4);
+            System.out.printf("Файл %s создан\n", fileNames[i]);
+        }
+        List<String> result = searchMatch(fileNames, TO_SEARCH);
+        for (String s: result) {
+            System.out.printf("Файл %s содержит искомое слово %s\n", s, TO_SEARCH);
+
+        }
+
+
+
     }
 
-    public static void outputFileSystem(File file, String indent, boolean isLast){
+    public static void outputFileSystem(File file, String indent, boolean isLast) {
         System.out.print(indent);
         if (isLast) {
             System.out.print("└");
             indent += " ";
-        }
-        else {
+        } else {
             System.out.print("├");
             indent += "│ ";
         }
         System.out.println(file.getName());
 
         File[] files = file.listFiles();
-        if(files == null) return;
+        if (files == null) return;
 
         int subDir = 0;
         for (int i = 0; i < files.length; i++) {
@@ -45,7 +62,7 @@ public class Program {
         }
         int subDirCounter = 0;
         for (int i = 0; i < files.length; i++) {
-            if (files[i].isDirectory()){
+            if (files[i].isDirectory()) {
                 outputFileSystem(files[i], indent, subDirCounter == subDir - 1);
                 subDirCounter++;
             }
@@ -85,38 +102,62 @@ public class Program {
 
         }
     }
-    private static void concatenate(String fileIn1, String fileIn2, String fileOut) throws IOException{
-        try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)){
+
+    private static void concatenate(String fileIn1, String fileIn2, String fileOut) throws IOException {
+        try (FileOutputStream fileOutputStream = new FileOutputStream(fileOut)) {
             int c;
-            try (FileInputStream fileInputStream = new FileInputStream(fileIn1)){
-                while ((c = fileInputStream.read()) != -1){
+            try (FileInputStream fileInputStream = new FileInputStream(fileIn1)) {
+                while ((c = fileInputStream.read()) != -1) {
                     fileOutputStream.write(c);
                 }
             }
-            try (FileInputStream fileInputStream = new FileInputStream(fileIn2)){
-                while ((c = fileInputStream.read()) != -1){
+            try (FileInputStream fileInputStream = new FileInputStream(fileIn2)) {
+                while ((c = fileInputStream.read()) != -1) {
                     fileOutputStream.write(c);
                 }
             }
         }
     }
-    private static boolean searchStrInFile(String filename, String search) throws IOException{
-        try(FileInputStream fileInputStream = new FileInputStream(filename)){
+
+    private static boolean searchStrInFile(String filename, String search) throws IOException {
+        try (FileInputStream fileInputStream = new FileInputStream(filename)) {
             byte[] searchData = search.getBytes();
             int c;
             int i = 0;
-            while ((c = fileInputStream.read()) != -1){
+            while ((c = fileInputStream.read()) != -1) {
                 if (c == searchData[i]) {
                     i++;
-                }
-                else {
+                } else {
                     i = 0;
                     if (c == searchData[i]) i++;
                 }
-                if ( i == searchData.length) return true;
+                if (i == searchData.length) return true;
             }
             return false;
 
         }
+    }
+
+    private static List<String> searchMatch(String[] files, String search) throws IOException {
+        List<String> list = new ArrayList<>();
+        File path = new File(new File(".").getCanonicalPath());
+        File[] dir = path.listFiles();
+        for (int i = 0; i < dir.length; i++) {
+            if (dir[i].isDirectory()) {
+                continue;
+            }
+            for (int j = 0; j < files.length; j++) {
+                if (dir[i].getName().equals(files[j])) {
+                    if (searchStrInFile(dir[i].getName(), search)) {
+                        list.add(dir[i].getName());
+
+                    }
+                }
+
+            }
+
+        }
+        return list;
+
     }
 }
